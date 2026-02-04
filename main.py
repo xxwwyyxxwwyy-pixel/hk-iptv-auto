0f0a39180eb84a9685734b51afb89eea11b1c09b
SWvs <59214670+sammy0101@users.noreply.github.com>
2026-02-04 18:59:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ BLOCK_KEYWORDS = [
     "Chopper", "Wow", "UHD", "8K", "Career", "Comics", "Movies",
     "CBTV","Pearl","AccuWeather","Jadeed","Curiosity","Electric",
     "Warfare","Knowledge","MagellanTV","70s","80s","90s","Rock",
-    "Winnipeg","Edmonton","RightNow","Times","True",
+    "Winnipeg","Edmonton","RightNow","Times","True","Mindanow",
     
     # 來自你的日誌分析 (大陸/澳門台)
     "浙江", "杭州", "西湖", "廣東", "珠江", "大灣區", # 排除 "杭州西湖明珠"
